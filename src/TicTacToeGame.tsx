b16a8e4ffc4385ef8d0aab0f97c2d12a818f0b3a
vengadeshraj <vengadeshraj405@gmail.com>
2025-10-23 10:11:53 +0530

    base setup implemented

---
+++ src/TicTacToeGame.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const TicTacToeGame = () => {
+  return <div className="flex flex-col items-center gap-5">
+    Hello from Tic Tac Toe Game
+  </div>;
+};
+
+export default TicTacToeGame;
---
+++ src/components/Card.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Card = () => {
+  return <div>Hello from Card</div>;
+};
+
+export default Card;
