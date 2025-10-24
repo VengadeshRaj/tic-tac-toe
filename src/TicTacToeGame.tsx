de3df6edaa78f88aff608c5a0fbdd5ddf46dcc36
vengadeshraj <vengadeshraj405@gmail.com>
2025-10-24 15:54:04 +0530

    Game complete

--- src/TicTacToeGame.tsx
+++ src/TicTacToeGame.tsx
@@ -18,8 +18,20 @@ const TicTacToeGame = () => {
     X: "O",
     O: "X",
   };
+
+  const PATTERNS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [1, 4, 7],
+    [2, 4, 6],
+    [0, 4, 8],
+    [0, 3, 6],
+    [2, 5, 8],
+  ];
   const [symbole, setSymbole] = useState<"X" | "O" | "">("X");
   const [cardValues, setCardValues] = useState<CardValue[]>([]);
+  const [isGameComplete, setIsGameComplete] = useState(false);
 
   useEffect(() => {
     const initialCardValues = [];
@@ -29,6 +41,26 @@ const TicTacToeGame = () => {
     setCardValues(initialCardValues);
   }, []);
 
+  const checkGameStatus = (newCardValues: CardValue[]): CardValue[] => {
+    for (let i = 0; i < PATTERNS.length; i++) {
+      const [a, b, c] = PATTERNS[i];
+      if (
+        newCardValues[a].symbole &&
+        newCardValues[a].symbole == newCardValues[b].symbole &&
+        newCardValues[a].symbole == newCardValues[c].symbole
+      ) {
+        newCardValues = newCardValues.map((card, index) => {
+          if (PATTERNS[i].includes(index)) {
+            return { ...card, isDone: true };
+          }
+          return card;
+        });
+        setIsGameComplete(true);
+      }
+    }
+    return newCardValues;
+  };
+
   const cardOnClick = (cardIndex: number) => {
     const newCardValues = cardValues.map((card, i: number) => {
       if (cardIndex == i) {
@@ -36,7 +68,9 @@ const TicTacToeGame = () => {
       }
       return card;
     });
-    setCardValues([...newCardValues]);
+    const checkedCardValues = checkGameStatus(newCardValues);
+    console.log("checkedCardValues", checkedCardValues);
+    setCardValues([...checkedCardValues]);
     setSymbole(SYBOLE_PAIR[symbole]);
   };
 
@@ -48,13 +82,17 @@ const TicTacToeGame = () => {
         isFrozen={card.isFrozen}
         index={i}
         onClick={(i: number) => cardOnClick(i)}
+        isGameComplete={isGameComplete}
       />
     ));
   return (
     <div className="flex flex-col items-center gap-5 font-bold text-3xl p-10 font-mono">
-      Tic Tac Toe!
+      <h1>Tic Tac Toe!</h1>
       <div className="flex flex-row gap-2 flex-wrap w-[350px]">
         {buildCards()}
+      </div>
+      <div>
+        {isGameComplete && <h1 className="lg:text-5xl animate-bounce p-10">Player {SYBOLE_PAIR[symbole]} Won!🎉</h1>}
       </div>
     </div>
   );
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,17 +6,18 @@ type CardProps = {
   onClick: (index: number) => void;
   isFrozen: boolean;
   isDone: boolean;
+  isGameComplete:boolean
 };
 
 const Card = (props: CardProps) => {
-  const { symbole, onClick, index, isFrozen, isDone } = props;
+  const { symbole, onClick, index, isFrozen, isDone, isGameComplete } = props;
   const cardContainerClass = isDone
     ? `flex w-[100px] h-[100px] bg-green-600 text-center items-center border-[2px] border-gray-500 rounded-md justify-center cursor-not-allowed`
     : `flex w-[100px] h-[100px] bg-gray-200 text-center items-center border-[2px] border-gray-500 rounded-md justify-center  ${
         isFrozen ? "cursor-not-allowed" : "cursor-pointer"} `;
 
   const cardOnClick = () => {
-    if (!isFrozen) onClick(index);
+    if (!isFrozen && !isGameComplete) onClick(index);
   };
 
   return (
